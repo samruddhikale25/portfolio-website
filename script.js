e88c03443e2b2c79cd22a835586dda4b119a5e76
samruddhikale25 <kalesamruddhi12@gmail.com>
2026-02-21 16:16:19 +0530

    Add files via upload
    Day 3 - Added projects section with modal popup

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const navLinks = document.getElementById("navLinks");
 menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+const modal = document.getElementById("projectModal");
+
+function openModal() {
+  modal.style.display = "flex";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target === modal) {
+    closeModal();
+  }
+};
